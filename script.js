45b12aef6004f19f8db316f794bd5f2387a7d089
Peterson Valadão <73299678+Petersonvaladao@users.noreply.github.com>
2022-07-24 22:58:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Primeira missão: fazer uma tela de login, que tenha login e senha, tanto login e senha devem ser "Admin" se algo for diferente disso, deve exibir uma mensagem falando que a senha e logo estão incorretos.
+
+const userName = document.querySelector("#user-name");
+const userPassword = document.querySelector("#password");
+const loginBtn = document.getElementById("btn");
+
+loginBtn.addEventListener("click", loginCheck);
+
+function loginCheck() {
+  if (userName.value != "Admin") {
+    alert("Nome de usuário incorreto. Favor ingresse nome válido");
+  } else if (userPassword.value != "Admin") {
+    alert("Senha incorreta. Tente novamente");
+  } else {
+    alert("Login efetuado com sucesso");
+  }
+}
+
+
+
